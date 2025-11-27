cccab41299bc7235492fb4240d0bc1857cc448f8
kharini06 <168970204+kharini06@users.noreply.github.com>
2025-11-27 10:47:35 +0530

    Update tests.py

--- F0-week8-master/tests.py
+++ F0-week8-master/tests.py
@@ -2,9 +2,9 @@ import unittest
 from main import to_upper
 class MyTestCase(unittest.TestCase): 
 def test_to_upper(self):
-name = "Likhi" 
+name = "Harini" 
 upper_name = to_upper(name)
-self.assertEqual(upper_name, "Tejaswi")
+self.assertEqual(upper_name, "Karuna")
 
 if __name	== '	main	': unittest.main() 
 
